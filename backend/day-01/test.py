b24ac382faf3e50841c19b4b73f80470cb4044a6
Cein-cein <vincegawen0@gmail.com>
2023-11-20 18:15:41 +0800

    Update test.py

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,8 @@
-name = None
-age = None
-isMale = None
-department = None
+name = 'Vince Wesley D. Gawen'
+age = 18
+isMale = 'male'
+department = 'Department of Software and Web Development'
+sport = 'basketball'
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in the {department}'s 30 Days of Coding.")
+print(f"He's athletic and plays {sport} when he's got free time.")
