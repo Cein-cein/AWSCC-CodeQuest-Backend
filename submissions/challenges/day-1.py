3492aa563a8d13b909fc321e97e493ba72bea956
Cein-cein <vincegawen0@gmail.com>
2023-11-21 17:57:37 +0800

    Update on the submission folder
    A folder that compiles the challenges I completed each day. Created for easy navigation of all the challenges. I edited the past two files and added a bit of a header to print both in the day-1.py and day-2.py files.

---
+++ submissions/challenges/day-1.py
@@ -0,0 +1,13 @@
+print("|===============================================================|",
+      "\n|=== Day 1: Variable Assignment and Printing Using F-Strings ===|",
+      "\n|==================== Programmed by: Vince =====================|"
+      "\n|===============================================================|\n")
+
+name = 'Vince Wesley D. Gawen'
+age = 18
+isMale = 'male'
+department = 'Department of Software and Web Development'
+sport = 'basketball'
+
+print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in the {department}'s 30 Days of Coding.")
+print(f"He's athletic and plays {sport} when he's got free time.")
---
+++ submissions/challenges/day-2.py
@@ -0,0 +1,12 @@
+print("|========================================|",
+      "\n|=== Day 2: Addition of Integer Input ===|",
+      "\n|========= Programmed by: Vince =========|"
+      "\n|========================================|\n")
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+sum = num1 + num2 + num3
+
+print(f"\nSum: {sum}")
